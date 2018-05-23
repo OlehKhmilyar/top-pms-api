4ec8016aad9bfc6d737dbc961ec12f063b07d208
Oleh Khmilyar <olegkhmilyar@gmail.com>
2018-05-23 18:22:07 +0300

    initialize top-pms-api

---
+++ src/main/java/com/example/TopPMSAPI/TopPmsApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.TopPMSAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopPmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopPmsApiApplication.class, args);
+	}
+}
